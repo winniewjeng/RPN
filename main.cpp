c3011221554b050ba67b9f781bc74d664b3be2fe
winniewjeng <winniewjeng@gmail.com>
2018-11-22 11:33:16 -0800

    housekeeping

--- main.cpp
+++ main.cpp
@@ -10,178 +10,24 @@
 double Eval(Queue<Token*> postfix);
 //parse the user-input string into tokens and store them in queue
 Queue<Token*> toToken(string expression);
+// check if eval is working
+void test_eval();
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    //    cout << "hello world" << endl;
-    //    Queue<int> q;
-    //    q.push(4);
-    //    cout << q.pop() << endl;
+//    test_eval();
     
     string infix;
     cout << "Enter the operation: ";
     cin >> infix;
     
-    // parse string into chars and store them inside a char queue
-    Queue<char> infixCharQ;
-    string num_str;
-    string trig_str;
-    Queue<Token*> input;
+    cout << toToken(infix) << endl;
     
-    for (int i = 0; i < infix.size(); i++) {
-        
-        if (isdigit(infix[i]) || infix[i] == '.') {
-            //form operand
-            while (isdigit(infix[i]) || infix[i] == '.') {
-                num_str+=infix[i];
-                i++;
-            }
-//            cout << num_str << "\n";
-            double number = stod(num_str);
-            input.push(new Operand(number));
-            num_str="";
-        }
-        
-//        cout << infix[i] << endl;
-        trig_str+= infix[i];
-        input.push(new Operator(trig_str));
-        trig_str = "";
-        //account for sine and cosine and tangent in this funciton too
-//        if (!isdigit(infix[i]) && infix[i] != '.') {
-//            trig_str = infix[i];
-//            while (!isdigit(infix[i]) && infix[i] != '.') {
-//                trig_str+=infix[i];
-//                i++;
-//            }
-//            cout << trig_str << "\n";
-//            input.push(new Operator(trig_str));
-//            trig_str="";
-//        }
-        
-//        else {
-//            cout << "her";
-//            cout << infix[i] << " ";
-//        }
-    }
-    
-    cout << endl;
-    cout << input << endl;
-    //    cout << infixCharQ << endl;
-    
-    
-    // parse the char queue, turn the numbers into numbers, and store them all inside a token queue
-    //    while( !infixCharQ.empty() ) {
-    //        cout << infixCharQ.top() << " ";
-    //
-    //        infixCharQ.pop();
-    //        if ( isdigit(infixCharQ.top()) || infixCharQ.top() == '.' ) {
-    //            //cout << infixCharQ.top() << " ";
-    //            num_str += infixCharQ.top();
-    //
-    //        } else {
-    //            cout << num_str << endl;
-    //            //convert num_string to double
-    //            double number = stod(num_str);
-    //            //create a new number token and push it to the token queue input
-    //            input.push(new Operand(number));
-    //            //resetting num_string
-    //            num_str = "";
-    //            //cout << " | " << infixCharQ.top() << " | ";
-    //            //push the top of the infixCharQueue also to the queue
-    //            input.push(new Operator(infixCharQ.top()));
-    //        }
-    //
-    //        infixCharQ.pop();
-    
-    //    }
-    //    cout << input << endl;
-    
-    
-    //    Queue<Token*> input;
-    //
-    //    input.push(new Operand(4));
-    //    input.push(new Operand(2));
-    //    input.push(new Operator('+'));
-    //    input.push(new Operand(5));
-    //    input.push(new Operator('*'));
-    //
-    //    cout << "Evaluate: " << endl;
-    //    cout << input << " = " << Eval(input) << endl;
-    //
-    //    Queue<Token*> input2;
-    //
-    //    input2.push(new Operand(8));
-    //    input2.push(new Operand(3));
-    //    input2.push(new Operator('-'));
-    //    input2.push(new Operand(5));
-    //    input2.push(new Operator('+'));
-    //    input2.push(new Operand(5));
-    //    input2.push(new Operator('-'));
-    //
-    //    cout << input2 << " = " << Eval(input2) << endl;
     
     return 0;
     
-    //    for (int i = 0; i < infix.size(); i++) {
-    //        //while the element in string is digit or decimal, get the whole number before tokenizing
-    //        double number_bank = 0;
-    //
-    //        while (isdigit(infix[i]) || infix[i] == '.') {
-    //            // if a decimal is detected
-    //            if (infix[i] == '.') {
-    //                if (number_bank == 0 && i == 0) {
-    //                    number_bank = 1;
-    //                } else {
-    //                    number_bank /= 10; }
-    //                cout << "number bank is "<< number_bank << endl;
-    //            }
-    //            // if a digit is detected
-    //            else {
-    //                cout << "i is "<< i << endl;
-    //                if(i == 0) {
-    //                    //if the digit is at the start of the string, it's a single digit
-    //                    number_bank = ((double)infix[i] - 48);
-    //
-    //                    cout << "is ! " << number_bank << endl;
-    //                } else if ((isdigit(infix[i-1])) || infix[i-1] == '.' ) {
-    //                    // if digit is trailing after another digit or a decimal, append it to the other number
-    //
-    //                    if (number_bank < 1) {
-    //                        cout << "numbre bank <1 before is "<< number_bank << endl;
-    //                        number_bank = number_bank * 10 + ( (double)infix[i] - 48) / 10;
-    //                        cout << "numbre bank <1 after is "<< number_bank << endl;
-    //                    }
-    //                    else {
-    //                        cout << "numbre bank >=1 after is "<< number_bank << endl;
-    //                        number_bank = number_bank * 10 + (double)infix[i] - 48;
-    //                        cout << "numbre bank >=1 after is "<< number_bank << endl;
-    //                    }
-    //                    cout << "numbre bank ais "<< number_bank << endl;
-    //                    cout << "ehh what "<< (double)infix[i] - 48 << endl;
-    ////                    cout << "&" << number_bank << endl;
-    //                } else {
-    //                    // if digit is not after another digit, it's a single digit
-    //                    number_bank = (double)infix[i] - 48;
-    //                }
-    //            }
-    //            //traverse down the string
-    //            i++;
-    //        }
-    //        cout << number_bank << endl;
-    
-    
-    //        int num_order = 1; // multiply tenfold each time a digit is followed by another digit
-    //        if(isdigit(infix[i])) {
-    //            double number = double(infix[i]);
-    //        } else if ()
-    
-    //    }
-    
-    //store the user input inside a Queue
-    //    Queue<Token*> infixQ = toToken(infix);
-    //    cout << infixQ;
     /*
      attack plan:
      //get an infix order string
@@ -199,29 +45,62 @@ int main(int argc, const char * argv[]) {
      */
 }
 
-//Queue<Token*> toToken(string expression) {
-//    //get an infixQ
-//    Queue<Token*> infixQ;
-//    //loop through user-input infix expression string
-//    for (int i = 0; i <expression.size(); i++) {
-//        // char element of the string is an operand (a number)
-//        if (isdigit(expression[i])) {
-//            cout << expression[i] << endl;
-//            //convert the char into double
-//            double digit = isdigit(expression[i]);
-//            cout << digit << endl;;
-//            //push the element into the infix Queue
-//            infixQ.push(new Operand(digit));
-//        }
-//        // char element of the string is an operator
-//        else {
-//            //push the element into the infix Queue
-//            infixQ.push(new Operator(expression[i]));
-//        }
-//        //        cout << infixQ << endl;
-//    }
-//    return infixQ;
-//}
+Queue<Token*> toToken(string infix) {
+    string num_str;
+    string optr_str; // operator string
+    Queue<Token*> input;
+    
+    for (int i = 0; i < infix.size(); i++) {
+        // if the item in string is a number, get the number and push it to input Queue
+        if (isdigit(infix[i]) || infix[i] == '.') {
+            //form operand
+            while (isdigit(infix[i]) || infix[i] == '.') {
+                num_str += infix[i];
+                i++;
+            }
+            double number = stod(num_str);
+            input.push(new Operand(number));
+            // reset the num_str for clean reading of the next number
+            num_str="";
+        }
+        
+        // if the item in string is an operator, get the operator and push it to input Queue
+        if (!isdigit(infix[i])) {
+            optr_str += infix[i];
+            input.push(new Operator(optr_str));
+            // reset the optr_str for clean reading of the next operator
+            optr_str = "";
+        }
+        
+        //trig detected
+        //if (isalpha(infix[i])) {}
+        
+    }
+    //    cout << endl;
+    //    cout << input << endl;
+    return input;
+    //    //get an infixQ
+    //    Queue<Token*> infixQ;
+    //    //loop through user-input infix expression string
+    //    for (int i = 0; i <expression.size(); i++) {
+    //        // char element of the string is an operand (a number)
+    //        if (isdigit(expression[i])) {
+    //            cout << expression[i] << endl;
+    //            //convert the char into double
+    //            double digit = isdigit(expression[i]);
+    //            cout << digit << endl;;
+    //            //push the element into the infix Queue
+    //            infixQ.push(new Operand(digit));
+    //        }
+    //        // char element of the string is an operator
+    //        else {
+    //            //push the element into the infix Queue
+    //            infixQ.push(new Operator(expression[i]));
+    //        }
+    //        //        cout << infixQ << endl;
+    //    }
+    //    return infixQ;
+}
 
 double Eval(Queue<Token*> postfix) {
     //a stack that stores double* Operands
@@ -257,5 +136,31 @@ double Eval(Queue<Token*> postfix) {
     }
     
     return s.top();
+};
+
+void test_eval() {
+    
+    Queue<Token*> input;
+    
+    input.push(new Operand(4));
+    input.push(new Operand(2));
+    input.push(new Operator("+"));
+    input.push(new Operand(5));
+    input.push(new Operator("*"));
+    
+    cout << "Evaluate: " << endl;
+    cout << input << " = " << Eval(input) << endl;
+    
+    Queue<Token*> input2;
+    
+    input2.push(new Operand(8));
+    input2.push(new Operand(3));
+    input2.push(new Operator("*"));
+    input2.push(new Operand(6));
+    input2.push(new Operator("/"));
+    input2.push(new Operand(2));
+    input2.push(new Operator("+"));
+    
+    cout << input2 << " = " << Eval(input2) << endl;
 };
 
